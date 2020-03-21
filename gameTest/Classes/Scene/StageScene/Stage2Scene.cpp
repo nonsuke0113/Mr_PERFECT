55ef0bbd89442c6cfb72c71dfdd8b0bc0e9010c8
masa0346 <62389674+masa0346@users.noreply.github.com>
2020-03-21 11:05:38 +0900

    ステージ２　初期位置、開始時メッセージ表示
    ステージ２　初期位置、開始時メッセージ表示

--- gameTest/Classes/Scene/StageScene/Stage2Scene.cpp
+++ gameTest/Classes/Scene/StageScene/Stage2Scene.cpp
@@ -41,12 +41,12 @@ void Stage2Scene::initMap()
 void Stage2Scene::initCharactors()
 {
     // プレイヤー
-    this->m_player = PlayerSprite::create("player.png", Vec2(7.0f, 16.0f), ::back, 0.1f);
+    this->m_player = PlayerSprite::create("player.png", Vec2(8.0f, 29.0f), ::back, 0.1f);
     this->m_player->setAnchorPoint(Vec2(0.0f, 0.0f));
     this->addChild(this->m_player);
     
     // 敵キャラクター
-    EnemySprite* mob = EnemySprite::create("enemy1.png", Vec2(7.0f, 19.0f), ::left, 0.1f, patorol_rotate);
+    EnemySprite* mob = EnemySprite::create("enemy1.png", Vec2(8.0f, 22.0f), ::front, 0.1f, patorol_rotate);
     mob->setName(StringUtils::format("mob"));
     mob->setAnchorPoint(Vec2(0.0f, 0.0f));
     this->addChild(mob);
@@ -55,7 +55,35 @@ void Stage2Scene::initCharactors()
 
 
 #pragma mark -
+#pragma mark ButtonEvent
+/**
+    Bボタン押下時に呼び出される処理
+    ステージ2では何もしない
+ */
+void Stage2Scene::touchB()
+{
+    return;
+}
+
+
+
+#pragma mark -
 #pragma mark GameEvent
+/**
+    ゲーム開始時の処理
+ */
+void Stage2Scene::gameStart()
+{
+    this->m_isTutorialMessage2 = false;
+    
+    // ミッション開始のメッセージ表示後、ゲームスタート
+    this->m_mdController->createStartSeekMissonMessage([this]() {
+        StageSceneBase::gameStart();
+        this->m_mdController->m_dialog->setCompleteAction(nullptr);
+    });
+}
+
+
 /**
     コンティニューを実行
  */
@@ -75,9 +103,17 @@ void Stage2Scene::doContinue()
  */
 void Stage2Scene::checkPosition()
 {
-    if (this->m_player->worldPosition() == Vec2(7.0f, 0.0f)) {
+    // クリア判定
+    if (this->m_player->worldPosition() == Vec2(8.0f, 20.0f)) {
         this->stageClear();
     }
+    
+    // メッセージ表示
+    if (this->m_player->worldPosition() == Vec2(8.0f, 26.0f)) {
+        // チュートリアルメッセージ2の表示
+        this->createTutorialMessage2();
+    }
+    
     return;
 }
 
@@ -93,4 +129,26 @@ void Stage2Scene::stageClear()
     
     // 親のクリア処理を呼び出す
     StageSceneBase::stageClear();
+}
+
+
+#pragma mark -
+#pragma mark Message
+/**
+    チュートリアルメッセージ2を表示する
+ */
+void Stage2Scene::createTutorialMessage2()
+{
+    if (this->m_isTutorialMessage2) {
+        return;
+    }
+    this->m_isTutorialMessage2 = true;
+    
+    std::vector<std::string> messages = std::vector<std::string>();
+    messages.push_back("「敵が道を塞いでいるな。」");
+    messages.push_back("「Aボタンで壁を叩いて、音で敵をひきつけよう。」");
+    this->m_mdController->setMessages(messages);
+    this->m_mdController->setMessageCallback([this](){
+    });
+    this->m_mdController->displayMessageDialog();
 }
