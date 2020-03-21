f0bf49135306a072696f546d800064c0cc882b76
まさしろ <>
2020-03-22 00:15:18 +0900

    コメントのインデントの修正

--- gameTest/Classes/Scene/StageScene/Stage5Scene.cpp
+++ gameTest/Classes/Scene/StageScene/Stage5Scene.cpp
@@ -10,7 +10,7 @@
 #pragma mark -
 #pragma mark Init
 /**
- シーンの作成
+    シーンの作成
  */
 Stage5Scene* Stage5Scene::createScene()
 {
@@ -26,7 +26,7 @@ Stage5Scene* Stage5Scene::createScene()
 
 
 /**
- MAPの初期化処理
+    MAPの初期化処理
  */
 void Stage5Scene::initMap()
 {
@@ -36,7 +36,7 @@ void Stage5Scene::initMap()
 
 
 /**
- キャラクターの初期化処理
+    キャラクターの初期化処理
  */
 void Stage5Scene::initCharactors()
 {
@@ -46,22 +46,22 @@ void Stage5Scene::initCharactors()
     this->addChild(this->m_player);
     
     // 敵キャラクター
-    EnemySprite* enemy1 = EnemySprite::create("enemy1.png", Vec2(8.0f, 23.0f), ::right, 0.1f, patorol_rotate);
+    EnemySprite* enemy1 = EnemySprite::create("enemy1.png", Vec2(8.0f, 23.0f), ::right, 0.1f, patorol_none);
     enemy1->setAnchorPoint(Vec2(0.0f, 0.0f));
     this->addChild(enemy1);
     
-    EnemySprite* enemy2 = EnemySprite::create("enemy1.png", Vec2(9.0f, 24.0f), ::left, 0.1f, patorol_rotate);
+    EnemySprite* enemy2 = EnemySprite::create("enemy1.png", Vec2(9.0f, 24.0f), ::left, 0.1f, patorol_none);
     enemy2->setAnchorPoint(Vec2(0.0f, 0.0f));
     this->addChild(enemy2);
     
-    EnemySprite* enemy3 = EnemySprite::create("enemy1.png", Vec2(12.0f, 25.0f), ::left, 0.1f, patorol_rotate);
+    EnemySprite* enemy3 = EnemySprite::create("enemy1.png", Vec2(12.0f, 25.0f), ::left, 0.1f, patorol_none);
     enemy3->setAnchorPoint(Vec2(0.0f, 0.0f));
     this->addChild(enemy3);
 }
 
 
 /**
- コンティニューを実行
+    コンティニューを実行
  */
 void Stage5Scene::doContinue()
 {
@@ -75,7 +75,7 @@ void Stage5Scene::doContinue()
 
 
 /**
- 座標の判定
+    座標の判定
  */
 void Stage5Scene::checkPosition()
 {
@@ -89,7 +89,7 @@ void Stage5Scene::checkPosition()
 
 
 /**
- ステージクリア時の処理
+    ステージクリア時の処理
  */
 void Stage5Scene::stageClear()
 {
